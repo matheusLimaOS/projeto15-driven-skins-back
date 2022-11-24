bc13d37540941a3506e03542519c894084229bbe
arthur999999 <arthurhono.contato@gmail.com>
2022-11-24 13:24:33 -0300

    bug fix

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,6 +3,6 @@ import { listAllProducts } from "../controllers/userControllers.js";
 
 
 const userRouter = Router ()
-userRouter.use('/listproducts', listAllProducts)
+userRouter.get('/listproducts', listAllProducts)
 
 export default userRouter
